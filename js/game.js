8754a6939a98fedde7444ca9f800519026c85633
hoppsikCZ <120632594+hoppsikCZ@users.noreply.github.com>
2023-12-01 11:49:12 +0100

    fix

--- js/game.js
+++ js/game.js
@@ -105,11 +105,11 @@ document.getElementById('bEndTurn').addEventListener('click', () => {
         alert("Ještě jsou na ploše kostky, které je možné označit!");
         return;
     } else if (vsechnyLocked) {
-        alert("Nelze ukončit tah, jsou-li všechny kostky uzamčeny! Házej znovu.");
-        return;
+            alert("Nelze ukončit tah, jsou-li všechny kostky uzamčeny! Házej znovu.");
+            return;
     } else if (score > 0 && score + currentScore >= 350) {
         skoreHracu[activePlayer] += currentScore + score;
-    } 
+    }
 
     kostky.forEach((item) => {
         if (item.dieDiv.getAttribute("data-locked") === "false")
