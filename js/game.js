345ba6d7837adf0299c3b594dc593e5766071d55
hoppsikCZ <120632594+hoppsikCZ@users.noreply.github.com>
2023-12-01 10:54:08 +0100

    Update game.js
    možná se to rozbije

--- js/game.js
+++ js/game.js
@@ -104,12 +104,12 @@ document.getElementById('bEndTurn').addEventListener('click', () => {
     if (calcSore(false) > score) {
         alert("Ještě jsou na ploše kostky, které je možné označit!");
         return;
-    } else if (score > 0 && score + currentScore >= 350) {
-        skoreHracu[activePlayer] += currentScore + score;
-    } else if (vsechnyLocked) {
+    else if (vsechnyLocked) {
         alert("Nelze ukončit tah, jsou-li všechny kostky uzamčeny! Házej znovu.");
         return;
-    }
+    } else if (score > 0 && score + currentScore >= 350) {
+        skoreHracu[activePlayer] += currentScore + score;
+    } 
 
     kostky.forEach((item) => {
         if (item.dieDiv.getAttribute("data-locked") === "false")
